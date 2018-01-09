f9bc8a5961fe2618dfb234723b9da17760d85703
Lucas Lingle <lucasdaxlingle@gmail.com>
2018-01-08 19:20:06 -0800

    memory-1 : unified handling of weight tying schemes

--- MemoryNetwork.py
+++ MemoryNetwork.py
@@ -1,10 +1,11 @@
 import tensorflow as tf
 
 
-
 class MemoryNetwork:
     def __init__(self, vocab_size, embedding_dim, number_of_hops,
-                 batch_size, number_of_memories, max_sentence_len):
+                 batch_size, number_of_memories, max_sentence_len,
+                 weight_tying_scheme='adj',
+                 position_encoding=True):
 
         self.V = vocab_size
         self.d = embedding_dim
@@ -14,71 +15,142 @@ class MemoryNetwork:
         self.M = number_of_memories
         self.J = max_sentence_len
 
-    def build_from_weight_scheme(self, weight_tying_scheme, input_sentences_ints_batch, u_batch):
-        answer_logits = None
+        self.nr_word_embedding_matrices_dict = {
+            'adj': 1 + (self.number_of_hops - 1) + 1,
+            'rnnlike': 1 + 2 + 1,
+            'allsame': 1,
+            'alldiff': 1 + (2 * self.number_of_hops) + 1
+        }
 
-        if weight_tying_scheme == 'adj':
-            answer_logits = self.build_from_adj_weight_scheme(input_sentences_ints_batch, u_batch)
+        self.nr_temporal_embedding_matrices_dict = {
+            'adj': 1 + (self.number_of_hops - 1) + 1,
+            'rnnlike': 2,
+            'allsame': 1,
+            'alldiff': (2 * self.number_of_hops)
+        }
 
-        if weight_tying_scheme == 'rnnlike':
-            answer_logits = self.build_from_rnnlike_weight_scheme(input_sentences_ints_batch, u_batch)
+        # formulas for retrieving the appropriate word embedding matrix in a list of distinct word embedding matrices
+        # the argument i assumes memory layers are indexed from 0 to K-1 (whereas the paper used 1-based indices)
+        self.routing_formula = {
+            'adj': {
+                'B': 0,
+                'A': lambda i: i,
+                'C': lambda i: i+1,
+                'W': -1
+            },
+            'rnnlike': {
+                'B': 0,
+                'A': lambda i: 1,
+                'C': lambda i: 2,
+                'W': -1
+            },
+            'allsame': {
+                'B': 0,
+                'A': lambda i: 0,
+                'C': lambda i: 0,
+                'W': 0
+            },
+            'alldiff': {
+                'B': 0,
+                'A': lambda i: i+1,
+                'C': lambda i: i+2,
+                'W': -1
+            }
+        }
 
-        return answer_logits
+        self.nr_word_embedding_matrices = self.nr_word_embedding_matrices_dict[weight_tying_scheme]
+        self.nr_temporal_embedding_matrices = self.nr_temporal_embedding_matrices_dict[weight_tying_scheme]
+
+        self.word_embedding_matrices_list = [self.build_word_embedding(idx) \
+                                             for idx in range(0, self.nr_word_embedding_matrices)]
+
+        self.temporal_embedding_matrices_list = [self.build_temporal_embedding(idx) \
+                                                 for idx in range(0, self.nr_temporal_embedding_matrices)]
+
+        self.layer_transition_operator = {
+            'adj': tf.constant(tf.eye(self.d)),
+            'rnnlike': self.build_H_mapping(),
+            'allsame': tf.constant(tf.eye(self.d)),
+            'alldiff': self.build_H_mapping()
+        }
+
+        self.H = self.layer_transition_operator[weight_tying_scheme]
+        self.l = self.build_position_encoding() if position_encoding else self.build_bag_of_words_encoding()
+
+        B_retrieval_idx = self.routing_formula[weight_tying_scheme]['B']
+        self.B = self.word_embedding_matrices_list[B_retrieval_idx]
+
+        W_retrieval_idx = self.routing_formula[weight_tying_scheme]['W']
+        self.W = self.word_embedding_matrices_list[W_retrieval_idx].T
+
+        sentences_ints_batch, question_ints_batch, answer_ints_batch = self.build_inputs()
+        u_batch = self.get_encoded_questions(question_ints_batch)
+
+        memory_output_batch = self.build_and_stack_memory_layers(
+            weight_tying_scheme,
+            sentences_ints_batch,
+            u_batch)
+
+        answer_logits_batch = tf.matmul(memory_output_batch, self.W)     # [batch_size, d] x [d, V] = [batch_size, V]
+        answer_probabilities_batch = tf.nn.softmax(answer_logits_batch)  # [batch_size, V]
 
 
-    def build_from_adj_weight_scheme(self, input_sentences_ints_batch, u_batch):
-        H = tf.eye(self.d)
 
-        A, C, l, T_A, T_C, _, u_next = self.build_memory_layer(input_sentences_ints_batch, u_batch, H=H)
+    def build_inputs(self):
+        sentences = tf.placeholder(tf.int32, [self.batch_size, self.M, self.J])
+        question = tf.placeholder(tf.int32, [self.batch_size, self.J])
+        answer = tf.placeholder(tf.int32, [self.batch_size])
+        return sentences, question, answer
 
-        for k in range(1, self.K):
-            A, C, _, T_A, T_C, _, u_next = self.build_memory_layer(input_sentences_ints_batch, u_next,
-                                                   A=C, C=None,
-                                                   l=l,
-                                                   T_A=T_C, T_C=None,
-                                                   H=H
-                                                   )
+    def get_encoded_questions(self, q_batch):
+        B_word_embeddings = tf.nn.embedding_lookup(self.B, q_batch)  # [batch_size, J, d]
 
-        W = self.build_W_mapping("W")
+        l_3dim = tf.expand_dims(self.l, 0)                           # [1, J, d]
+        B_word_embeddings = B_word_embeddings * l_3dim               # [batch_size, J, d] x [1, J, d] = [batch_size, J, d]
 
-        answer_logits = tf.matmul(u_next, W) # [batch_size, d] x [d, V] = [batch_size, V]
-        return answer_logits
+        u_batch = tf.reduce_sum(B_word_embeddings, 1)                # [batch_size, d]
 
-    def build_from_rnnlike_weight_scheme(self, input_sentences_ints_batch, u_batch):
+        return u_batch
 
-        A, C, l, T_A, T_C, H, u_next = self.build_memory_layer(input_sentences_ints_batch, u_batch)
+    def build_and_stack_memory_layers(self, weight_tying_scheme, input_sentences_ints_batch, u_batch):
+        layer_results = [u_batch]
 
-        for k in range(1, self.K):
-            _, _, _, _, _, _, u_next = self.build_memory_layer(input_sentences_ints_batch, u_next,
-                                                   A=A, C=C,
-                                                   l=l,
-                                                   T_A=T_A, T_C=T_C,
-                                                   H=H
-                                                   )
+        for i in range(0, self.K):
+            A_retrieval_idx = self.routing_formula[weight_tying_scheme]['A'](i)
+            A = self.word_embedding_matrices_list[A_retrieval_idx]
 
-        W = self.build_W_mapping("W")
+            C_retrieval_idx = self.routing_formula[weight_tying_scheme]['C'](i)
+            C = self.word_embedding_matrices_list[C_retrieval_idx]
 
-        answer_logits = tf.matmul(u_next, W) # [batch_size, d] x [d, V] = [batch_size, V]
-        return answer_logits
+            T_A_retrieval_idx = self.routing_formula[weight_tying_scheme]['T_A'](i)
+            T_A = self.word_embedding_matrices_list[T_A_retrieval_idx]
+
+            T_C_retrieval_idx = self.routing_formula[weight_tying_scheme]['T_C'](i)
+            T_C = self.word_embedding_matrices_list[T_C_retrieval_idx]
+
+            u_next = self.build_memory_layer(
+                input_sentences_ints_batch=input_sentences_ints_batch,
+                u_batch=layer_results[-1],
+                A=A,
+                C=C,
+                T_A=T_A,
+                T_C=T_C
+            )
+
+            layer_results.append(u_next)
+
+        return layer_results[-1]
 
     def build_memory_layer(self, input_sentences_ints_batch, u_batch,
-                           A=None, C=None,
-                           l=None,
-                           T_A=None, T_C=None,
-                           H=None):
-        A = tf.identity(A) if (A is not None) else self.build_memory_embedding("A_" + str(0))
-        C = tf.identity(C) if (C is not None) else self.build_memory_embedding("C_" + str(0))
+                           A, C, T_A, T_C):
 
         A_word_embeddings = tf.nn.embedding_lookup(A, input_sentences_ints_batch)  # [batch_size, M, J, d]
-        C_word_embeddings = tf.nn.embedding_lookup(C, input_sentences_ints_batch)   # [batch_size, M, J, d]
+        C_word_embeddings = tf.nn.embedding_lookup(C, input_sentences_ints_batch)  # [batch_size, M, J, d]
 
-        l = tf.identity(l) if (l is not None) else self.build_position_encoding("l_encoding")  # [1, 1, J, d]
+        l_4dim = tf.expand_dims(tf.expand_dims(self.l, 0), 0)                      # [1, 1, J, d]
 
-        A_word_embeddings *= l
-        C_word_embeddings *= l
-
-        T_A = tf.identity(T_A) if (T_A is not None) else self.build_temporal_embedding("T_A_" + str(0))
-        T_C = tf.identity(T_C) if (T_C is not None) else self.build_temporal_embedding("T_C_" + str(0))
+        A_word_embeddings = A_word_embeddings * l_4dim    # [batch_size, M, J, d] x [1, 1, J, d] = [batch_size, M, J, d]
+        C_word_embeddings = C_word_embeddings * l_4dim    # [batch_size, M, J, d] x [1, 1, J, d] = [batch_size, M, J, d]
 
         m_A_without_temporal = tf.reduce_sum(A_word_embeddings, 2) # [batch_size, M, d]
         m_C_without_temporal = tf.reduce_sum(C_word_embeddings, 2) # [batch_size, M, d]
@@ -96,31 +168,24 @@ class MemoryNetwork:
         o = tf.matmul(p, m_C)                              # [batch_size, 1, M] x [batch_size, M, d] = [batch_size, 1, d]
         o = tf.squeeze(o, [1])                             # [batch_size, d]
 
-        H = tf.identity(H) if (H is not None) else self.build_H_mapping("H_" + str(0))
+        u_next = tf.matmul(o, self.H) + u_batch            # [batch_size, d]
 
-        u_next = tf.matmul(H, o) + u_batch
+        return u_next
 
-        return A, C, l, T_A, T_C, H, u_next
+    def build_word_embedding(self, idx):
+        embedding_matrix = tf.get_variable('word_embedding_matrix_' + str(idx), dtype='float', shape=[self.V, self.d])
+        return embedding_matrix
 
-    def build_word_embedding(self, variable_scope_name, reuse=False):
-        with tf.variable_scope(variable_scope_name, reuse=reuse):
-            embedding_matrix = tf.get_variable('embedding_matrix', dtype='float', shape=[self.V, self.d])
-            return embedding_matrix
+    def build_temporal_embedding(self, idx):
+        embedding_matrix = tf.get_variable('temporal_embedding_matrix_' + str(idx), dtype='float', shape=[self.M, self.d])
+        return embedding_matrix
 
-    def build_temporal_embedding(self, variable_scope_name, reuse=False):
-        with tf.variable_scope(variable_scope_name, reuse=reuse):
-            embedding_matrix = tf.get_variable('embedding_matrix', dtype='float', shape=[self.M, self.d])
-            return embedding_matrix
+    def build_H_mapping(self):
+        H_matrix = tf.get_variable('H_matrix', dtype='float', shape=[self.d, self.d])
+        return H_matrix
 
-    def build_H_mapping(self, variable_scope_name, reuse=False):
-        with tf.variable_scope(variable_scope_name, reuse=reuse):
-            embedding_matrix = tf.get_variable('embedding_matrix', dtype='float', shape=[self.d, self.d])
-            return embedding_matrix
-
-    def build_W_mapping(self, variable_scope_name, reuse=False):
-        with tf.variable_scope(variable_scope_name, reuse=reuse):
-            embedding_matrix = tf.get_variable('embedding_matrix', dtype='float', shape=[self.d, self.V])
-            return embedding_matrix
+    def build_bag_of_words_encoding(self):
+        return tf.constant(tf.ones([self.J, self.d]))
 
     def build_position_encoding(self):
         def l_kj(k, j):
@@ -129,21 +194,9 @@ class MemoryNetwork:
         def l_j(j):
             return [l_kj(k, j) for k in range(1, self.d + 1)]
 
-        l = [l_j(j) for j in range(1, self.J + 1)]
+        l_list = [l_j(j) for j in range(1, self.J + 1)]
 
-        l_tensor = tf.constant(l, shape=[self.J, self.d], name='l')
+        l = tf.constant(l_list, shape=[self.J, self.d], name='l')
 
-        l_withMdim = tf.expand_dims(l_tensor, 0, name='l_withMdim')
-        l_withMdim_withbatchdim = tf.expand_dims(l_withMdim, 0, name='l_withMdim_withbatchdim')
-
-        return l_withMdim_withbatchdim
-
-
-
-
-
-
-
-
-
+        return l
 
